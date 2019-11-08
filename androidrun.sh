ac40ae66c6a0ada9b955264b3582a904f0769997
Alex <alex2005.uy@gmail.com>
2019-11-08 13:01:14 -0300

    CSS mejorado

---
+++ androidrun.sh
@@ -0,0 +1,4 @@
+export ANDROID_SDK_ROOT=$HOME/Library/Android/sdk
+export ANDROID_HOME="/home/alex/Android/Sdk"
+ionic cordova run android
+
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: 'http://192.168.1.44:3000'
+  url: 'http://192.168.1.46:3000'
 };
